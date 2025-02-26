f66a8c3a0568c6be8f4feef59d51598187342ccb
Malte Hillebrand <maltehillebrand96@gmail.com>
2025-02-26 15:42:33 +0100

    added mockup timeline and imitated adobe styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import './style.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, Webpack + React!</h1>
-      <p>This is your first React-powered Adobe CEP extension.</p>
-      <p>Now with hot Reloading!</p>
-    </div>
+    <main>
+        <div id='settings'>
+            <label for='bpm'>BPM</label><input name='bpm' type='number' size='3' /> <button>set</button>
+        </div>
+      <hr />
+        <div id='timeline'>
+            <div className='grid-layers'>
+                <div className='timeline-layer'>MyAnimation</div>
+                <div className='timeline-layer'>MyAnimation2</div>
+            </div>
+            <div className='grid-bar'>
+                <div className='grid-beat'>
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                </div>
+                <div className='grid-beat'>
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                </div>
+                <div className='grid-beat'>
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                    <div className='grid-beat-fill' />
+                </div>
+            </div>
+        </div>
+    </main>
   );
 };
 
