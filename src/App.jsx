51f420fa8c79de336a8a7b085540123e99348012
Malte Hillebrand <maltehillebrand96@gmail.com>
2025-02-27 16:09:37 +0100

    added layer functionality mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useState, useRef, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import './style.scss'
 
 const App = () => {
 
     const [bpm, setBpm] = useState(120.0);
-    const bpmInput = useRef(null);
 
     const handleBpmFormChange = (e) => {
         setBpm(e.target.value);
     }
     
-    const setBpmFromForm = () => {
-        setBpm(bpmInput.current.value)
+    const [beatsPerBar, setBeatsPerBar] = useState(4);
+
+    const handleBeatsPerBarFormChange = (e) => {
+        setBeatsPerBar(e.target.value);
     }
 
     const [zoomLevel, setZoomLevel] = useState(1.0);
@@ -27,7 +28,7 @@ const App = () => {
 
     const beatsPerSecond = bpm / 60;
     const totalBeats = useMemo(() => Math.floor(dummyCompData.duration * beatsPerSecond), [bpm]);
-    const beatsPerBar = 4;
+    const roundedBeats = useMemo(() => Math.ceil(totalBeats / beatsPerBar) * beatsPerBar, [bpm]);
     const totalBars = Math.ceil(totalBeats / beatsPerBar);
 
     const dummyLayerData  = [ {
@@ -53,25 +54,53 @@ const App = () => {
   return (
     <main>
         <div id='settings'>
-            <label htmlFor='bpm'>BPM</label><input name='bpm' type='number' size='3' value={bpm} onChange={handleBpmFormChange} ref={bpmInput}/> <button onClick={setBpmFromForm}>set</button>
+            <label htmlFor='bpm'>BPM</label><input name='bpm' type='number' size='3' value={bpm} onChange={handleBpmFormChange} />
+            <select name='beatsPerBar' id='beatsPerBarSelect' onChange={handleBeatsPerBarFormChange}>
+                <option value='2'>2</option>
+                <option value='4' selected>4</option>
+                <option value='8'>8</option>
+                <option value='16'>16</option>
+            </select>
             <button onClick={increaseZoom}>zoom in</button>
             <button onClick={decreaseZoom}>zoom out</button>
         </div>
       <hr />
         <div id='timeline'>
-            <div className='grid-layers'  style={{gridTemplate: `100% / repeat(${totalBars * beatsPerBar}, 1fr)`, width: `${100 * zoomLevel}%`}}>
-                <div className='timeline-layer'>MyAnimation</div>
-                <div className='timeline-layer'>MyAnimation2</div>
+            <div className='grid-layers'  style={{gridTemplate: `100% / repeat(${roundedBeats}, 1fr)`, width: `${100 * zoomLevel}%`}}>
+                {dummyLayerData.map((layer, index) => {
+
+                    const gridStart = Math.floor(layer.inPoint / dummyCompData.duration * totalBeats) + 1;
+                    const gridEnd = Math.floor(layer.outPoint / dummyCompData.duration * totalBeats) + 1;
+
+                    return (
+                        <div key={index} className='timeline-layer' style={{ gridColumnStart: gridStart, gridColumnEnd: gridEnd, gridRow: index + 1 }}>
+                            {layer.name}
+                        </div>
+                    );
+                })}
             </div>
             <div className='grid-timeline' style={{gridTemplate: `100% / repeat(${totalBars}, 1fr)`, width: `${100 * zoomLevel}%`}}>
-                {Array.from({ length: totalBars }).map((_, barIndex) => (
+                {Array.from({ length: totalBars }).map((_, barIndex) => {
+                    return (
                     <div key={barIndex} className='grid-bar' style={{gridTemplate: `100% / repeat(${beatsPerBar}, 1fr)`}}>
-                        {Array.from({ length: beatsPerBar }).map((_, beatIndex) => (
-                            <div key={beatIndex} className='grid-beat' />
-                        ))}
+                        {Array.from({ length: beatsPerBar }).map((_, beatIndex) => {
+
+                            const currentTotalBeats= ((barIndex ) * beatsPerBar) + (beatIndex + 1);
+
+                            if(currentTotalBeats <= totalBeats) {
+                                    console.log("currentTotalBeats", currentTotalBeats);
+                                    return(<div key={beatIndex} className='grid-beat' />);
+                            }
+                            
+                        })}
                     </div>
-                ))}
+                )
+            })}
             </div>
+        </div>
+        <hr />
+        <div id='information'>
+            <p><small>comp duration: {dummyCompData.duration} seconds | total beats: {totalBeats} | total bars: {totalBars} | beats per second: {beatsPerSecond}</small></p>
         </div>
     </main>
   );
